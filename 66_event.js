2e76c9559605e051bc28e7e393c12956b363b4ae
Gagan <gagank2199@gmail.com>
2023-06-21 22:36:13 +0530

    addeventlistner & removeeventlistner

--- 66_event.js
+++ 66_event.js
@@ -1,8 +1,8 @@
 document.getElementById('1st').addEventListener("mouseleave",abc);
-document.getElementById('1st').removeEventListener("click",xyz)
-document.getElementById('1st').removeEventListener("dblclick",function(){
-    document.getElementById('1st').style.backgroundColor="red";
-})
+document.getElementById('1st').removeEventListener("click",xyz);
+document.getElementById('2nd').addEventListener("click",function(){
+   this.style.backgroundColor="red";
+});
 function abc(){
     document.getElementById('1st').style.backgroundColor="black";
     document.getElementById('1st').style.color="white";
