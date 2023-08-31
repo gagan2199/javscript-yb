029583ed6b0d763f080f1e536907d1e1d7badbf3
Gagan <gagank2199@gmail.com>
2023-08-31 21:59:54 +0530

    dom

--- dom1.js
+++ dom1.js
@@ -1,7 +1,7 @@
 var a;
 var c;
 c=document;
-a= document.getElementById("3p").innerHTML="<h3> good morning </h3>"
+a= document.getElementById("3p").innerHTML="<h3> greetings </h3>"
 console.log(a);
 document.write("abc");
 console.log(c);
